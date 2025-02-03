b6daeff1a50d17b7116b3291522bb94d7f36f6d7
Gorney-Alex <gorneyalexandr@gmail.com>
2025-02-03 10:51:34 +0300

    RoadCreatorScript is finished

--- ScriptsGame/CreatorRoad/CreatorRoads.cs
+++ ScriptsGame/CreatorRoad/CreatorRoads.cs
@@ -1,37 +1,101 @@
-using System.Collections;
+// Gorney-Alex code
+
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CreatorRoads : MonoBehaviour
 {
+    [SerializeField] private int maxRoadsInWorld = 15;
     [SerializeField] private GameObject[] roadArrayVersions = new GameObject[5];
-    // StraightRoad[0], RoadBelow[1], Road90[2], Road90Twice[3], RoadAbove[4]
+    // StraightRoad[0], RoadBelow[1], RoadAbove[2], Road90Twice[3], Road90[4]
 
     private List<GameObject> roadList = new List<GameObject>();
 
+    private Vector3 nextPositionRoad = new Vector3(0f, 0f, 0f);
 
-    private Vector3 nextPositionRoad = new Vector3(0, 0, 0);
-    private int width = 100;
-    private int height = 200;
-    private int maxRoads = 5;
+    private bool isMaxRoadReached = false;
 
 
     private void Start()
     {
-        CreateRoad();
+        CreateOneStarightRoad();
     }
 
     private void Update()
-    {
-        
+    {   
+        if (!isMaxRoadReached)
+        {
+            if (MaxCountRoads()) 
+            {
+                isMaxRoadReached = true;
+                CreatorTheLast5Roads();
+                Debug.Log("Max count of roads reached!");
+            } 
+            else {
+                CreateRandomRoads();
+            }
+        }
     }
 
-    private void CreateRoad()
+    private void CreateOneStarightRoad()
     {
-        for (int i = 0; i < maxRoads; i++)
+        GameObject newRoad = Instantiate(roadArrayVersions[0], nextPositionRoad, Quaternion.identity);
+        newRoad.name = "0";
+        roadList.Add(newRoad);
+        DistanceDeterminationZ();
+    }
+
+    private void CreatorTheLast5Roads()
+    {
+        for(int i = 0; i < 5; i++)
         {
-            GameObject newRoad = Instantiate(roadArrayVersions[i], nextPositionRoad, Quaternion.Euler(0, 0, 0));
-            nextPositionRoad += new Vector3(0, 0, height);
+            CreateOneStarightRoad();
         }
+    }
+
+    private void CreateRandomRoads()
+    {
+        int randomIndexRoad = Random.Range(0, 5);
+        GameObject newRoad = Instantiate(roadArrayVersions[randomIndexRoad], nextPositionRoad, Quaternion.identity);
+        newRoad.name = randomIndexRoad.ToString();
+        roadList.Add(newRoad);
+        DistanceDeterminationZ();
+    }
+
+    private void DistanceDeterminationZ()
+    {
+        float width = 22f;
+        float length = 200f;
+        float length90Twice = 400f;
+
+        GameObject lastRoad = roadList[roadList.Count - 1];
+
+        switch (lastRoad.name)
+        {
+            case "0":
+                nextPositionRoad += new Vector3(0f, 0f, length);
+                break;
+            case "1":
+                nextPositionRoad += new Vector3(0f, -width, length);
+                break;
+            case "2":
+                nextPositionRoad += new Vector3(0f, width, length);
+                break;
+            case "3":
+                nextPositionRoad += new Vector3(length90Twice, 0f, length90Twice);
+                break;
+            case "4":
+                nextPositionRoad += new Vector3(-length90Twice, 0f, length90Twice);
+                break;
+            default:
+                Debug.Log("error in DistanceDeterminationZ");
+                break;
+        }
+    }
+
+    private bool MaxCountRoads()
+    {
+        if(roadList.Count >= maxRoadsInWorld) { return true; }
+        return false;
     }
 }
